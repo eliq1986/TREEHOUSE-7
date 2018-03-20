0bd2c74cf78636947e0f294e0407642db0c138aa
Elijah Quesada <this_boy10@yahoo.com>
2018-03-19 18:38:45 -0700

    Display letters

--- app.js
+++ app.js
@@ -4,61 +4,62 @@ const startGame = document.querySelector("a");
 const phraseDisplay = document.querySelector("ul");
 const overlay = document.getElementById("overlay");
 const keyButton = document.getElementsByTagName("button");
-const phrasesArr = ["abcdefg", "may the force be with you", "much to learn you still have", "join the dark side we have cookies", "use the force"]
+const phrasesArr = ["abcde", "May the force be with you", "Much to learn you still have", "Join the dark side we have cookies", "Use the force"]
 let missed = 0;
 
 startGame.addEventListener("click", function() { // removes startup screen
-  overlay.style.display = "none";
+    overlay.style.display = "none";
 });
 
 function randomNumber() { //Generates random number
-return Math.floor(Math.random() * 5);
+    return Math.floor(Math.random() * 5);
 }
 
-function getRandomPhraseArray(arr) {  //gets random phrase and turns it into an array of letters
-   let splitArray =  arr[randomNumber()];
-   return splitArray = splitArray.split([,]);
+function getRandomPhraseArray(arr) { //gets random phrase and turns it into an array of letters
+    let splitArray = arr[randomNumber()];
+    return splitArray = splitArray.split([, ]);
 }
 const phraseArray = getRandomPhraseArray(phrasesArr); //array of letters gets captured by a variable
 
 function addPhraseToDisplay(arr) { //adds random phrease to display
-  for (let i = 0; i < arr.length; i++) {
-      let li = document.createElement("li"); //creates an "li" element for each letter
-      li.textContent = arr[i];
-      phraseDisplay.appendChild(li);
+    for (let i = 0; i < arr.length; i++) {
+        let li = document.createElement("li"); //creates an "li" element for each letter
+        li.textContent = arr[i];
+        phraseDisplay.appendChild(li);
 
-    if (li.textContent !== " ") {
-       li.className = "letter";
-  }
-  else {
-  li.className = "space";
+        if (li.textContent !== " ") {
+            li.className = "letter";
+        } else {
+            li.className = "space";
 
-  }
-}
+        }
+    }
 }
 
 function checkLetter(buttonClicked) {
- const elementsWithClass = document.getElementsByClassName("letter");
+    const elementsWithClass = document.getElementsByClassName("letter");
     for (let i = 0; i < elementsWithClass.length; i++) {
-          if (elementsWithClass[i].textContent == buttonClicked.textContent) {
-               let correctElement = elementsWithClass[i];
-                correctElement.className = "show";
-
-           }
+        if (buttonClicked.textContent.toLowerCase() === elementsWithClass[i].textContent.toLowerCase()) {
+             elementsWithClass[i].className = "show";
 
 
-       }
-}
+
+      }
+    }
+  }
+
+
+
 
 qwertyDiv.addEventListener("click", (event) => {
-       if (event.target.tagName === "BUTTON") {
-         event.target.className = "chosen";
+    if (event.target.tagName === "BUTTON") {
           const button = event.target;
-          const right = checkLetter(button);
+          button.className = "chosen";
+         let letterFound = checkLetter(button);
          button.disabled = "true";
-          let letterFound = button;
-          return letterFound;
+         return letterFound;
+       }
+  }
 
-            }
-          });
+});
 addPhraseToDisplay(phraseArray);
