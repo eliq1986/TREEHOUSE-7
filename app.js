b50f9eb67e19773a3df5671b99ad1fbdddc0bfd2
Elijah Quesada <this_boy10@yahoo.com>
2018-03-20 15:00:23 -0700

    Lose/Win completed

--- app.js
+++ app.js
@@ -3,28 +3,25 @@ const startGame = document.querySelector("a");
 const phraseDisplay = document.querySelector("ul");
 const overlay = document.getElementById("overlay");
 
-const phrasesArr = ["abcde",
-                    "may the force be with you",
-                    "much to learn you still have",
-                    "join the dark side we have cookies",
-                    "use the force"];
+const phrasesArr = ["Do or do not",
+                    "May the force be with you",
+                    "Much to learn you still have",
+                    "Join the dark side we have cookies",
+                    "Use the force"];
 
 let missed = 0;
 
 startGame.addEventListener("click", () => { // removes startup screen
     overlay.style.display = "none";
 });
-
 function randomNumber() { //Generates random number
     return Math.floor(Math.random() * 5);
 }
-
 function getRandomPhraseArray(arr) { //gets random phrase and turns it into an array of letters
     let splitArray = arr[randomNumber()];
     return splitArray = splitArray.split([ , ]);
 }
-
- const phraseArray = getRandomPhraseArray(phrasesArr); //array of letters gets captured by a variable
+const phraseArray = getRandomPhraseArray(phrasesArr); //array of letters gets captured by a variable
 
 function addPhraseToDisplay(arr) { //adds random phrease to display
     for (let i = 0; i < arr.length; i++) {
@@ -36,14 +33,13 @@ function addPhraseToDisplay(arr) { //adds random phrease to display
         } else {
             li.className = "space";
                 }
-
-    }
-  }
+        }
+ }
 function checkLetter(buttonClicked) {
     let letter;
     const elementsWithClass = document.getElementsByClassName("letter");
     for (let i = 0; i < elementsWithClass.length; i++) {
-        if (elementsWithClass[i].textContent === buttonClicked.textContent) {
+        if (elementsWithClass[i].textContent.toLowerCase() === buttonClicked.textContent.toLowerCase()) {
            elementsWithClass[i] = elementsWithClass[i].classList.add("show");
              letter = elementsWithClass[i];
  }
@@ -69,11 +65,23 @@ qwertyDiv.addEventListener("click", (event) => {
 const lettersWithClassLetter= document.getElementsByClassName("letter").length;
 const lettersWithClassShow = document.getElementsByClassName("show").length;
 
-
   if (lettersWithClassShow === lettersWithClassLetter) {
-    console.log("You win");
+      const h2 = document.querySelector("h2");
+      const a = document.querySelector("a");
+        a.style.display = "none";
+        h2.textContent = "You Win!!!!";
+        overlay.style.display = "block";
+        overlay.classList.add("win");
+}
+else if (missed >= 5){
+  const lose = document.querySelector("h2");
+  const a = document.querySelector("a");
+  a.style.display = "none";
+  lose.textContent = "Try again";
+  overlay.style.display = "block";
+  overlay.classList.add("lose");
+
 }
   });
 
-
-  addPhraseToDisplay(phraseArray);
+addPhraseToDisplay(phraseArray);
